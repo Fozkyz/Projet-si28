8bd703524dd86a845c9fedf19ad8bd6bb6c65ba6
Fozkyz <fozkyz@gmail.com>
2021-06-05 17:03:29 +0200

    Parallax effect changes

--- Projet-SI28/Assets/Scripts/Parallax.cs
+++ Projet-SI28/Assets/Scripts/Parallax.cs
@@ -6,7 +6,7 @@ public class Parallax : MonoBehaviour
 {
 	[SerializeField] GameObject cam;
     [SerializeField] float parallax_effect;
-	
+
 	float length;
 	Vector3 start_pos;
 
@@ -14,13 +14,17 @@ public class Parallax : MonoBehaviour
 	{
 		start_pos = transform.position;
 		length = GetComponent<SpriteRenderer>().bounds.size.x;
+		GameObject go =Instantiate(gameObject, transform.position + Vector3.left * length, Quaternion.identity, transform);
+		Destroy(go.GetComponent<Parallax>());
+		go = Instantiate(gameObject, transform.position + Vector3.right * length, Quaternion.identity, transform);
+		Destroy(go.GetComponent<Parallax>());
 	}
 
 	void Update()
 	{
-		float distx = (cam.transform.position.x * parallax_effect);
-		float disty = (cam.transform.position.y * parallax_effect * .1f);
-		float temp = (cam.transform.position.x * (1 - parallax_effect));
+		float distx = cam.transform.position.x * parallax_effect;
+		float disty = cam.transform.position.y * 0.95f;
+		float temp = cam.transform.position.x * (1 - parallax_effect);
 
 		transform.position = start_pos + Vector3.right * distx + Vector3.up * disty;
 		
